047e17cded032a9dfc63aae7885011ad0a188207
Jeroen Ebus <jeroen.ebus@peoplerock.nl>
2023-10-16 21:53:46 +0200

    Renamed products

--- Remove_Teams_Personal_Install_Teams_Work/Remove_MSTeams_Personal_Install_MSTeams_Work_School.ps1
+++ Remove_Teams_Personal_Install_Teams_Work/Remove_MSTeams_Personal_Install_MSTeams_Work_School.ps1
@@ -1,22 +1,22 @@
 ## Check if Microsoft Teams (Personal) is available and Remove the app when possible.
 
 if ($null -eq (Get-AppxPackage -Allusers -Name MicrosoftTeams)) {
-    Write-Host "Microsoft Teams Client not available"
+    Write-Host "Microsoft Teams (Personal) not available"
 }
 Else {
     Get-AppxPackage -AllUsers -Name MicrosoftTeams | Remove-AppxPackage -AllUsers -ErrorAction Stop
-    Write-Host "Removed Microsoft Teams"  
+    Write-Host "Removed Microsoft Teams (Personal)"  
 }
 
 
 ## Install Microsoft Teams Machine Wide 64-bit version
 
 if (Test-Path "C:\Program Files (x86)\Teams Installer") {
-    Write-Host "Teams Machine Wide installer is already installed"
+    Write-Host "Micorosoft Teams Machine Wide 64-bit is already installed"
     Exit 0
 }
 Else { 
     start-process "Teams_windows_x64.msi" -Wait -ArgumentList "ALLUSERS=1"
-    Write-Host "Installed Microsoft Teams"
+    Write-Host "Installed Microsoft Teams Machine Wide 64-bit"
     Exit 0
 }
